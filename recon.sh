db74cf7a3dd239f9ed716c977a4841735c318b2b
LightningDarkMatter <70206474+LightningDarkMatter@users.noreply.github.com>
2023-01-04 15:23:37 +0000

    Tool using case function

--- recon.sh
+++ recon.sh
@@ -6,17 +6,25 @@ DOMAIN=$1
 DIRECTORY=${DOMAIN}_recon
 echo "Creating directory $DIRECTORY."
 mkdir $DIRECTORY
-if [ $2 == "nmap-only" ]
-then
-    nmap $DOMAIN > $DIRECTORY/nmap
-    echo "The results of nmap scan are stored in $DIRECTORY/nmap."
-elif [ $2 == "dirsearch-only" ]
-then
-    PATH_TO_DIRSEARCH/dirseach.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
-    echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
-else
-    nmap $DOMAIN > $DIRECTORY/nmap
-    echo "The results of nmap scan are stored in $DIRECTORY/nmap."
-    PATH_TO_DIRSEARCH/dirseach.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
-    echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
-fi
+case $2 in
+    nmap-only)
+        nmap $DOMAIN > $DIRECTORY/nmap
+        echo "The results of nmap scan are stored in $DIRECTORY/nmap."\
+        ;;
+    dirsearch-only
+        $PATH_TO_DIRSEARCH/dirseach.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
+        echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
+        ;;
+    crt-only)
+        curl "https://crt.sh/?=q$DOMAIN&output=json" -o $DIRECTORY/crt
+        echo "The results of cert parsing is stored in $DIRECTORY/crt."
+        ;;
+    *)
+        nmap $DOMAIN > $DIRECTORY/nmap
+        echo "The results of nmap scan are stored in $DIRECTORY/nmap."
+        $PATH_TO_DIRSEARCH/dirseach.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
+        echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
+        curl "https://crt.sh/?=q$DOMAIN&output=json" -o $DIRECTORY/crt
+        echo "The results of cert parsing is stored in $DIRECTORY/crt."
+        ;;
+esac
