4c7f4f720188e3c1d4a57a571aa8bd2d90c9abe6
LightningDarkMatter <70206474+LightningDarkMatter@users.noreply.github.com>
2023-01-04 15:28:20 +0000

    Creating functions for each tool to refactor the code.

--- recon.sh
+++ recon.sh
@@ -6,25 +6,34 @@ DOMAIN=$1
 DIRECTORY=${DOMAIN}_recon
 echo "Creating directory $DIRECTORY."
 mkdir $DIRECTORY
+nmap_scan()
+{
+    nmap $DOMAIN > $DIRECTORY/nmap
+    echo "The results of nmap scan are stored in $DIRECTORY/nmap."
+}
+dirsearch_scan()
+{
+    $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
+    echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
+}
+crt_scan()
+{
+    curl "https://crt.sh/?=q$DOMAIN&output=json" -o $DIRECTORY/crt
+    echo "The results of cert parsing is stored in $DIRECTORY/crt."
+}
 case $2 in
     nmap-only)
-        nmap $DOMAIN > $DIRECTORY/nmap
-        echo "The results of nmap scan are stored in $DIRECTORY/nmap."\
+        nmap_scan
         ;;
     dirsearch-only
-        $PATH_TO_DIRSEARCH/dirseach.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
-        echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
+        dirsearch_scan
         ;;
     crt-only)
-        curl "https://crt.sh/?=q$DOMAIN&output=json" -o $DIRECTORY/crt
-        echo "The results of cert parsing is stored in $DIRECTORY/crt."
+        crt_scan
         ;;
     *)
-        nmap $DOMAIN > $DIRECTORY/nmap
-        echo "The results of nmap scan are stored in $DIRECTORY/nmap."
-        $PATH_TO_DIRSEARCH/dirseach.py -u $DOMAIN -e php --simple-report=$DIRECTORY/dirsearch
-        echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
-        curl "https://crt.sh/?=q$DOMAIN&output=json" -o $DIRECTORY/crt
-        echo "The results of cert parsing is stored in $DIRECTORY/crt."
+        nmap_scan
+        dirsearch_scan
+        crt_scan
         ;;
 esac
